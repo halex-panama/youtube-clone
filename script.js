49193846cf82310037b27b2361576e64f08f0514
alexx155 <halex.panama@gmail.com>
2023-12-06 18:39:45 +0700

    add video container

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let menuIcon = document.querySelector(".menu-icon");
 let navbar = document.querySelector(".navbar");
+let videoContainer = document.querySelector(".video-container")
 
 menuIcon.onclick = function(){
   navbar.classList.toggle("active");
+  videoContainer.classList.toggle("shrink")
 }
