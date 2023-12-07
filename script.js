8552a9042a62992cec19cc99aad5a59e5207ed7c
alexx155 <halex.panama@gmail.com>
2023-12-07 19:34:49 +0700

    add mobile responsive

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let menuIcon = document.querySelector(".menu-icon");
-let navbar = document.querySelector(".navbar");
-let videoContainer = document.querySelector(".video-container")
+let menuIcon = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar');
+let videoContainer = document.querySelector('.video-container');
+let overlay = document.querySelector('.overlay')
 
 menuIcon.onclick = function(){
-  navbar.classList.toggle("active");
-  videoContainer.classList.toggle("shrink")
+  navbar.classList.toggle('active');
+  videoContainer.classList.toggle('shrink');
+  overlay.classList.toggle('show');
+}
+
+overlay.onclick = function(){
+  navbar.classList.remove('active');
+  overlay.classList.remove('show')
 }
