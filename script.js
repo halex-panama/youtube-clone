f70eb27959a6301f698c52df2d22c83bc982f247
alexx155 <halex.panama@gmail.com>
2023-12-14 18:28:58 +0700

    using video grid js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-let menuIcon = document.querySelector('.menu-icon'),
-    header = document.querySelector('header'),
-    mobile = document.querySelector('.mobile'),
-    themeButton = document.querySelector('.theme'),
-    navbar = document.querySelector('.navbar'),
-    videoContainer = document.querySelector('.video-container'),
-    overlay = document.querySelector('.overlay'),
-    showMoreButton = document.querySelector('.show-more'),
-    subMenu = document.querySelector ('.sub-menu');
+const menuIcon = document.querySelector('.menu-icon'),
+      navbar = document.querySelector('.navbar'),
+      videoContainer = document.querySelector('.video-container'),
+      overlay = document.querySelector('.overlay'),
+      showMoreButton = document.querySelector('.show-more'),
+      subMenu = document.querySelector ('.sub-menu');
 
 menuIcon.addEventListener('click', () => {
   navbar.classList.toggle('active');
@@ -23,31 +20,35 @@ showMoreButton.addEventListener('click', () => {
   subMenu.classList.toggle('open');
 });
 
-themeButton.addEventListener('click', () => {
-  header.classList.toggle('dark-mode');
-  mobile.classList.toggle('dark-mode');
-  navbar.classList.toggle('dark-mode');
-  videoContainer.classList.toggle('dark-mode');
+const themeClasses = document.querySelectorAll('header, .navbar, .video-container');
+
+document.querySelector('.theme').addEventListener('click', () => {
+  themeClasses.forEach(element => {
+    element.classList.toggle('dark-mode');
+  });
+})
+
+let videoGridHTML = '';
+
+videoGrid.forEach((video, index) => {
+  videoGridHTML += `
+  <div class="video-grid">
+    <div class="thumbnail-container">
+      <img src="${videoGrid[index].thumbnail}">
+      <div class="video-time">${videoGrid[index].time}</div>
+    </div>
+    <div class="info-container">
+      <div class="channel-picture">
+        <img src="${videoGrid[index].picture}">
+      </div>
+      <div class="text-container">
+        <p class="video-title">${videoGrid[index].title}</p>
+        <p class="channel-name">${videoGrid[index].name}</p>
+        <p class="views">${videoGrid[index].views}</p>
+      </div>
+    </div>
+</div>
+  `;
 });
 
-// menuIcon.onclick = function(){
-//   navbar.classList.toggle('active');
-//   videoContainer.classList.toggle('shrink');
-//   overlay.classList.toggle('show');
-// }
-
-// overlay.onclick = function(){
-//   navbar.classList.remove('active');
-//   overlay.classList.remove('show')
-// }
-
-// showMoreButton.onclick = function(){
-//   subMenu.classList.toggle('open');
-// }
-
-// themeButton.onclick = function(){
-//   header.classList.toggle('dark-mode');
-//   mobile.classList.toggle('dark-mode');
-//   navbar.classList.toggle('dark-mode');
-//   videoContainer.classList.toggle('dark-mode');
-// }
+document.querySelector('.video-container').innerHTML = videoGridHTML;
---
+++ videoGrid.js
@@ -0,0 +1,98 @@
+const videoGrid = [
+  {
+    thumbnail: 'assets/thumbnail/thumbnail-1.jpg',
+    title : 'Witnessing the Majestic Sun at the Edge of the World',
+    name: 'Halex Panama',
+    picture: 'assets/channel-picture/channel-picture-1.jpg',
+    views: '2.2M views &#183; 5 years ago',
+    time: '4.00'
+  },
+  {
+    thumbnail: 'assets/thumbnail/thumbnail-2.jpg',
+    title : 'Illuminate Your Path: The Mysterious Glow of the Lamp on Dark Streets',
+    name: 'Halex Panama',
+    picture: 'assets/channel-picture/channel-picture-2.jpg',
+    views: '316 views &#183; 7 days ago',
+    time: '3.52'
+  },
+  {
+    thumbnail: 'assets/thumbnail/thumbnail-3.jpg',
+    title : 'Embracing Solitude: The Enchanting Sunset Stroll Down the Deserted Street',
+    name: 'Halex Panama',
+    picture: 'assets/channel-picture/channel-picture-3.jpg',
+    views: '985K views &#183; 7 months ago',
+    time: '22.03'
+  },
+  {
+    thumbnail: 'assets/thumbnail/thumbnail-4.jpg',
+    title : "Dancing Silhouette: A Majestic Deer's Morning Serenade in the Wild Forest",
+    name: 'Halex Panama',
+    picture: 'assets/channel-picture/channel-picture-4.jpg',
+    views: '1K views &#183; 8 years ago',
+    time: '7.21'
+  },
+  {
+    thumbnail: 'assets/thumbnail/thumbnail-5.jpg',
+    title : 'The Infinity Stones: Unleashing the Power of the Avengers',
+    name: 'Halex Panama',
+    picture: 'assets/channel-picture/channel-picture-5.jpg',
+    views: '2.2M views &#183; 5 years ago',
+    time: '19.23'
+  },
+  {
+    thumbnail: 'assets/thumbnail/thumbnail-6.jpg',
+    title : 'Natures Symphony: Enchanting Mountainscapes and Tranquil Waterscapes',
+    name: 'Halex Panama',
+    picture: 'assets/channel-picture/channel-picture-6.jpg',
+    views: '985K views &#183; 7 months ago',
+    time: '00.47'
+  },
+  {
+    thumbnail: 'assets/thumbnail/thumbnail-7.jpg',
+    title : 'Aqua Architecture: Unveiling the Wonders of Water in Built Structures',
+    name: 'Halex Panama',
+    picture: 'assets/channel-picture/channel-picture-2.jpg',
+    views: '767K views &#183; 9 weeks ago',
+    time: '30.56'
+  },
+  {
+    thumbnail: 'assets/thumbnail/thumbnail-8.jpg',
+    title : 'Shadows of Mystery: Captivating Close-ups in the Enigmatic Depths of Black',
+    name: 'Halex Panama',
+    picture: 'assets/channel-picture/channel-picture-4.jpg',
+    views: '19 views &#183; 3 hours ago',
+    time: '30.56'
+  },
+  {
+    thumbnail: 'assets/thumbnail/thumbnail-9.jpg',
+    title : 'Starry Night Symphony: Exploring the Celestial Symphony of Space and Astronomy',
+    name: 'Halex Panama',
+    picture: 'assets/channel-picture/channel-picture-1.jpg',
+    views: '327K views &#183; 3 years ago',
+    time: '00.56'
+  },
+  {
+    thumbnail: 'assets/thumbnail/thumbnail-10.jpg',
+    title : 'Dancing Horizons: Enchanting Sunset Paints the Sky and Water in Spectacular Cloudy Strokes',
+    name: 'Halex Panama',
+    picture: 'assets/channel-picture/channel-picture-1.jpg',
+    views: '5.5M views &#183; 11 months ago',
+    time: '29.31'
+  },
+  {
+    thumbnail: 'assets/thumbnail/thumbnail-11.jpg',
+    title : 'Crimson Chaos: An Intimate Encounter with a Shattered Interior',
+    name: 'Halex Panama',
+    picture: 'assets/channel-picture/channel-picture-6.jpg',
+    views: '5.5M views &#183; 11 months ago',
+    time: '02.00.31'
+  },
+  {
+    thumbnail: 'assets/thumbnail/thumbnail-12.jpg',
+    title : "The Enigmatic Chronicles of the Whispering Willow: Unveiling the Secrets of Nature's Mystical Guardian",
+    name: 'Halex Panama',
+    picture: 'assets/channel-picture/channel-picture-3.jpg',
+    views: '19 views &#183; 3 hours ago',
+    time: '9.31'
+  }
+];
